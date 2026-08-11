ad90bfa6beaef459f5648d92b7cec511ba46507a
Mayank Pandey <mkp7905@gmail.com>
2026-08-11 08:52:50 +0530

    updated code

--- router.py
+++ router.py
@@ -30,4 +30,38 @@ def two_sum_optimal_approach( nums: list[int], target: int) -> list[int]:
         return [preMap[diff], i]
      preMap[num] = i
      return []
-      
+
+
+#Reomve duplicate from sorted array
+
+#brute force approach using a set
+#time:O(n log n)
+#space:O(n)
+@app.post("/remove_duplicates_brute_force")
+def remove_duplicates_brute_force(nums: list[int]):
+
+   unique = list(set(nums))
+   unique.sort()
+
+   for i in range(len(unique)):
+        nums[i] = unique[i]
+        return len(unique)
+
+
+@app.post("/remove_duplicates_optimal_approach")
+def remove_duplicates_optimal_approach(nums: list[int]):
+    # """
+    # Optimal approach to remove duplicates from a sorted array.
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    # """
+    if not nums:
+        return 0
+
+    left=0
+    for right in range(1, len(nums)):
+        if nums[right] != nums[left]:
+            left += 1
+            nums[left] = nums[right]
+
+    return left + 1
